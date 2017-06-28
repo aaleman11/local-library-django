e5cad168c45f036b044e97e3cf003827c3c61051
aaleman11 <aaleman11@gmail.com>
2017-06-28 16:02:11 -0700

    Generate Book Mapping

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -5,6 +5,9 @@
 from django.conf.urls import url
 from . import views
 
+#^ is a string start marker and $ is an end of string marker
 urlpatterns = [
-url(r'^$', views.index, name='index')       #Will call views.index is pattern is detected
+    url(r'^$', views.index, name='index'),       #Will call views.index is pattern is detected
+    url(r'^books/$', views.BookListView.as_view(), name='books'),
+    url(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
 ]
